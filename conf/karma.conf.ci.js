77f593cdb76f9ebd529822c9fa5dc9b64ed1248e
ghudiczius <github@jmk.hu>
2014-11-10 20:45:14 +0100

    added karma configuration

---
+++ conf/karma.conf.ci.js
@@ -0,0 +1,30 @@
+var baseConfig = require('./karma.conf.js');
+
+module.exports = function(config) {
+	// load base config
+	baseConfig(config);
+
+	// override base config
+	config.set({
+		frameworks : ['jasmine', 'requirejs'],
+		preprocessors : {
+			'test/**/*Spec.js' : ['coverage']
+		},
+		coverageReporter : {
+			reporters : [{
+					type : 'text-summary'
+				}, {
+					type : 'html',
+					dir : 'coverage'
+				}, {
+					type : 'json',
+					dir : 'coverage'
+				}
+			]
+		},
+		reporters : ['progress', 'coverage'],
+		colors : false,
+		autoWatch : false,
+		singleRun : true
+	});
+};
